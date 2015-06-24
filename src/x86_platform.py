d61f3f9d9557194901063bd41c103f3f85fbfa01
schernetsky <serg@amigocloud.com>
2015-06-23 18:21:24 -0700

    Updated x86 to use libc++ in c11 mode

--- src/x86_platform.py
+++ src/x86_platform.py
@@ -1,11 +1,15 @@
 from platform import Platform
+import amigo_config
 
 
 class X86Platform(Platform):
     def __init__(self, arch):
         super(X86Platform, self).__init__("x86", arch)
+        cflags = " -fPIC -pipe -isysroot /"
+        cxxflags = cflags
+        if amigo_config.CXX11:
+            cxxflags += " -stdlib=libc++"
         self.append_default_flags('LDFLAGS',
-                                  " -fpic -Wl," +
-                                  "-rpath-link=/usr/lib -L/usr/lib")
-        self.append_default_flags('CFLAGS', " -fPIC -pipe -isysroot /")
-        self.append_default_flags('CXXFLAGS', " -fPIC -pipe -isysroot /")
+                                  " -fpic -L/usr/lib")
+        self.append_default_flags('CFLAGS', cflags)
+        self.append_default_flags('CXXFLAGS', cxxflags)
